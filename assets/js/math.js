f9628aa9c15651384a8c44db1040b996bf908acd
Kevin D <kevinduong306@gmail.com>
2025-12-10 19:13:57 -0500

    Deployed 35e0d42 with MkDocs version: 1.6.1

---
+++ assets/js/math.js
@@ -0,0 +1,9 @@
+document$.subscribe(() => {
+    renderMathInElement(document.body, {
+        delimiters: [
+            { left: "$$", right: "$$", display: true },
+            { left: "\\(", right: "\\)", display: false },
+            { left: "$", right: "$", display: false }
+        ]
+    });
+});
